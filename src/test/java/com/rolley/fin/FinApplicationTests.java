fdb6d32fe0a713dd26c675cd4ea3110f6d15115f
rolley <rolleymatter@gmail.com>
2021-11-27 15:17:38 +0800

    Initial commit

---
+++ src/test/java/com/rolley/fin/FinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolley.fin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
